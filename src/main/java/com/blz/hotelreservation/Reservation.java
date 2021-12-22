beadd6bad13683169a08e5d3ccbd46fd5257e2ac
Manishakahar <manisha.kahar1998@gmail.com>
2021-12-22 08:26:43 +0530

    Added: Welcome Message

---
+++ src/main/java/com/blz/hotelreservation/Reservation.java
@@ -0,0 +1,7 @@
+package com.blz.hotelreservation;
+
+public class Reservation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation System");
+    }
+}
